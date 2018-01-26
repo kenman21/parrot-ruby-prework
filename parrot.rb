f5d6708163d1ebde269896a15aa1d80d57bdfb3a
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-01-26 04:48:56 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(string = "Squack!")
-  puts "Squack!"
-  "Squack!"
+def parrot(string = "Squawk!")
+  puts "Squawk!"
+  "Squawk!"
 end 
